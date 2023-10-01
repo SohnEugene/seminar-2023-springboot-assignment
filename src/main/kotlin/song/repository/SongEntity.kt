b0c2c7e8675df7ebc9179fcae67d0d1366c359c6
Eugene Sohn <qeugene2328@gmail.com>
2023-10-02 00:27:39 +0900

    song, songartist 엔티티 생성
    "property getter or setter expected"오류?

---
+++ src/main/kotlin/song/repository/SongEntity.kt
@@ -0,0 +1,30 @@
+package com.wafflestudio.seminar.spring2023.song.repository
+
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+
+@Entity(name = "songs")
+class SongEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+    val title: String,
+    val duration: Int,
+
+    @ManyToOne // default FetchType.EAGER
+    @JoinColumn(name = "album_id")
+    val album: AlbumEntity,
+    //album과의 연결 관계 형성
+
+    @OneToMany(mappedBy = "song")
+    val song_artists: List<SongArtistEntity>,
+
+    @OneToMany(mappedBy = "song")
+    val playlist_songs: List<PlaylistSongEntity>,
+}
